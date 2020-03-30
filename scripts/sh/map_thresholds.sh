a07774683b86942e26d9084e93b04446a8b1eb19
Barbarossa <barbarossav@vuw.leidenuniv.nl>
2020-03-30 16:05:44 +0200

    update sh scripts

--- scripts/sh/map_thresholds.sh
+++ scripts/sh/map_thresholds.sh
@@ -6,4 +6,7 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=vbarbarossa@science.ru.nl
 
+module load 2019
+module load R/3.5.1-foss-2018b
+
 Rscript scripts/R/map_thresholds.R
--- scripts/sh/model_occurrence_dispersal.sh
+++ scripts/sh/model_occurrence_dispersal.sh
@@ -7,4 +7,7 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=vbarbarossa@science.ru.nl
 
+module load 2019
+module load R/3.5.1-foss-2018b
+
 Rscript scripts/R/extra/model_occurrence_dispersal.R
