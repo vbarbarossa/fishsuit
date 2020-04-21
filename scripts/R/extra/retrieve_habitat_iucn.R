b6f4db70368d2908d74fc8c3d638ae94be81c8a8
Barbarossa <barbarossav@vuw.leidenuniv.nl>
2020-04-21 14:41:42 +0200

    m

--- scripts/R/extra/retrieve_habitat_iucn.R
+++ scripts/R/extra/retrieve_habitat_iucn.R
@@ -14,7 +14,7 @@ token <- 'd361026f05b472e57b0ffe1fa5c9a768aaf3d8391abbb464293e9efe2bbbf733'
 sp_names <- sf::read_sf('proc/species_ranges_merged.gpkg') %>% .$binomial
 
 # retrieve the habitat type per species
-for(i in sp_names[1:3]) {
+for(i in sp_names) {
   
   hab <- rl_habitats(as.character(i),key = token)$result$habitat
   saveRDS(hab,paste0(dir_habitat,i,'.rds'))
