602f8e94181b51fcf57507d3fae07f098a99677a
Barbarossa <barbarossav@vuw.leidenuniv.nl>
2020-04-21 14:39:02 +0200

    mod

--- scripts/R/extra/retrieve_habitat_iucn.R
+++ scripts/R/extra/retrieve_habitat_iucn.R
@@ -1,8 +1,9 @@
 # RU CLUSTER --> could not install rredlist on cartesius
 
 # retrieve habitat type
-library(rredlist)
 source('config.R');
+
+libinv(c('sf','dplyr','rredlist'))
 
 dir_habitat <- dir_(paste0('proc/iucn_habitat_list/'))
 
@@ -13,7 +14,7 @@ token <- 'd361026f05b472e57b0ffe1fa5c9a768aaf3d8391abbb464293e9efe2bbbf733'
 sp_names <- sf::read_sf('proc/species_ranges_merged.gpkg') %>% .$binomial
 
 # retrieve the habitat type per species
-for(i in sp_names) {
+for(i in sp_names[1:3]) {
   
   hab <- rl_habitats(as.character(i),key = token)$result$habitat
   saveRDS(hab,paste0(dir_habitat,i,'.rds'))
