392105d4030f86de9ec55a0c013403ccebc6f0b0
Barbarossa <barbarossav@vuw.leidenuniv.nl>
2020-04-22 19:05:38 +0200

    m

--- scripts/R/xtr/threshold_selection.R
+++ scripts/R/xtr/threshold_selection.R
@@ -2,34 +2,27 @@ source('config_local.R'); # always load as functions are loaded within this scri
 
 libinv(c('dplyr'))
 
+# species ranges
+ranges <- read_sf('proc/species_ranges_merged.gpkg') %>%
+  as_tibble() %>% select(-geom)
+
+# # # # load species habitat type <<<<<<< need to check why we are losing almost 100 species when merging
+hab <- left_join(ranges,read.csv('proc/species_traits.csv'))
+
 for(g in seq_along(climate_models)){
   
   # override vars
   vars <- c('Qmi','Qma','Qzf','Tma','Tmi')
   thresholds <- c('2.5','100.0','97.5','97.5','2.5')
   clmod = climate_models[g]
-  scen = scenarios[scenarios != 'hist'] #<
-  years = floor(mean(timespan_scen)) #<
   
-  dir_niche <- paste0(dir_model,'proc/',clmod,'/niches/')
+  dir_niche <- paste0('proc/',clmod,'/niches/')
   
-  # # # # load iucn data
-  iucn <- lapply(1:2, function(i) foreign::read.dbf(paste0('data/FW_FISH_20181113/FW_FISH_PART_',i,'.dbf'))) %>%
-    do.call('rbind',.)
-  iucn <- iucn[,c('id_no','binomial')][!duplicated(iucn[,c('id_no','binomial')]),]
-  row.names(iucn) <- 1:nrow(iucn)
-  
-  # # # # load species habitat type <<<<<<< need to check why we are losing almost 100 species when merging
-  hab <- merge(iucn,read.csv('iucn_habitat_type.csv'),by='binomial')
   
   niche_list <- list()
   for(j in seq_along(vars)){
     niche <- readRDS(paste0(dir_niche,vars[j],'.rds'))
-      
     
-    # remove ranges with NAs
-    # remove ranges with SD = 0, as they are sampled for only one 5 arcmin cell
-    # niche <- niche[!is.na(niche$mean),] # & niche$sd != 0
     niche <- niche[,c('ID',paste0(thresholds[j],'%'))]
     colnames(niche)[2] <- paste0(vars[j],'_',colnames(niche)[2])
     
@@ -42,7 +35,7 @@ for(g in seq_along(climate_models)){
   ))
   colnames(niche) <- sapply(niche_list,function(x) colnames(x)[2])
   niche <- cbind(niche_list[[1]][,1],niche)
-  colnames(niche)[1] <- 'IUCN_ID'
+  colnames(niche)[1] <- 'id_no'
   
   #---------------------------------------------------------------------------------------------
   # FILTERING
@@ -52,7 +45,6 @@ for(g in seq_along(climate_models)){
   
   # STEP 0 ###
   cat('STEP 0: initial species no. = ',nrow(niche),'\n')
-  saveRDS(niche$IUCN_ID,paste0(dir_niche,'filtering_step0_IDs.rds'))
   
   # STEP 1 ###
   # filter out species outside the pcrglobwb domain and
@@ -61,28 +53,19 @@ for(g in seq_along(climate_models)){
   step1 <- d[!is.na(d$mean) & d$no.grids >= min_no_grid_cells,1]
   
   cat('STEP 1: removing ',(nrow(niche) - length(step1)),' species represented by less than ',min_no_grid_cells,' grid-cell or falling out of the pcr-globwb domain\n')
-  niche <- niche[niche$IUCN_ID %in% step1,]
-  saveRDS(niche$IUCN_ID,paste0(dir_niche,'filtering_step1_IDs.rds'))
+  niche <- niche[niche$id_no %in% step1,]
   
-  niches_f <- niche
   
-  if(filter_lentic_out){
-    
-    # STEP 2
-    ## merge habitat
-    niches <- merge(niche,hab[,c('id_no','binomial','lotic','lentic')],by.x='IUCN_ID',by.y='id_no')
-    cat('removing ',(nrow(niche) - nrow(niches)),' species when merging with habitat info\n')
-    saveRDS(niches$IUCN_ID,paste0(dir_niche,'/filtering_step2_IDs.rds'))
-    
-    # STEP 3
-    ## filter for lotic sp only
-    niches_f <- niches[(niches$lotic == 1),]
-    cat('removing ',(nrow(niches) - nrow(niches_f)),' species exclusively lentic\n')
-    saveRDS(niches_f$IUCN_ID,paste0(dir_niche,'/filtering_step3_IDs.rds'))
-    
-  }
+  # STEP 2
+  ## merge habitat
+  niche <- left_join(niche %>% as_tibble,hab %>% mutate(id_no = as.character(id_no)))
   
-  niches_f <- niches_f[niches_f$`Tmi_2.5%` > 273.15,]
+  # STEP 3
+  ## filter for lotic sp only
+  niches_f <- niche[niche$lentic_only == 0,]
+  cat('removing ',(nrow(niche) - nrow(niches_f)),' species exclusively lentic\n')
+  
+  niches_f <- niches_f[niches_f$`Tmi_2.5%` > 273.15,] #<<<<<<<<<< WHAT? rather set it to this value where it is lower?
   
   write.csv(niches_f,paste0(dir_niche,'niches_filtered.csv'),row.names = F)
   
@@ -96,8 +79,8 @@ libinv(c('ggplot2','purrr','tidyr','bestNormalize','foreach'))
 tab <- niches_f %>%
   as_tibble()
 
-apply(tab %>% select(-IUCN_ID),2,function(x) sum(is.na(x)))
-apply(tab %>% select(-IUCN_ID),2,function(x) sum(x == 0))
+apply(tab %>% select(-id_no),2,function(x) sum(is.na(x)))
+apply(tab %>% select(-id_no),2,function(x) sum(x == 0))
 
 
 # check distribution
