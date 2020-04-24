541c04806d7becd056ae076548e3a77f15735858
Barbarossa <barbarossav@vuw.leidenuniv.nl>
2020-04-24 18:25:03 +0200

    m

--- scripts/sh/model_occurrence_dispersal.sh
+++ scripts/sh/model_occurrence_dispersal.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -t 1:00:00
-#SBATCH -p normal
 #SBATCH --array 1-5
+#SBATCH -t 20:00:00
+#SBATCH -p normal
 #SBATCH --output=diag/model_occurrence_dispersal_%a.out
 #SBATCH --mail-type=END
 #SBATCH --mail-user=vbarbarossa@science.ru.nl
@@ -10,4 +10,4 @@
 module load 2019
 module load R/3.5.1-foss-2018b
 
-Rscript scripts/R/extra/model_occurrence_dispersal.R
+Rscript scripts/R/model_occurrence_dispersal.R
